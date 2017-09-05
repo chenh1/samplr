1a19d1c8b7876516e6a2deb596a89a0201fc1ca7
Howard Chen <chenh0886@gmail.com>
2017-09-05 01:43:13 -0700

    Add path for web socket to heroku

--- src/apiPath.js
+++ src/apiPath.js
@@ -6,4 +6,7 @@ const subscriptionPath = window.location.hostname === 'localhost' ?
     'ws://' + window.location.hostname + ':4000/' :
     'ws://desolate-peak-60507.herokuapp.com/';
 
+//const apiPath = window.location.protocol + '//desolate-peak-60507.herokuapp.com/';
+//const subscriptionPath = 'ws://desolate-peak-60507.herokuapp.com/';
+
 export { apiPath, subscriptionPath };

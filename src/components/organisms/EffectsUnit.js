a343134c38fbad2532283467189e943c47597287
Howard Chen <chenh0886@gmail.com>
2017-10-19 01:16:15 -0700

    Add todo note

--- src/components/organisms/EffectsUnit.js
+++ src/components/organisms/EffectsUnit.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const EffectsUnit = (props) => {
-    console.log(props);
+    /*
+        TODO:
+        - Set filter/slider/spinners
+        - Each filter to have effect id
+        - Each filter to have key attribute to map to state
+        - Each filter to have value to pass to state
+    */
     return (
         <div>
             {props.type}
